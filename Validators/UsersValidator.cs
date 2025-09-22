ede53749f178b9f9e7f725109124f876f0d8151e
felipethd <felipe_s_lima@msn.com>
2025-09-22 19:45:24 -0300

    Adicionando FluentValidation no login

---
+++ Validators/UsersValidator.cs
@@ -0,0 +1,19 @@
+﻿using BackendTraining.Dtos;
+using FluentValidation;
+
+namespace BackendTraining.Validators
+{
+    public class UsersValidator : AbstractValidator<LoginDto>
+    {
+        public UsersValidator()
+        {
+            RuleFor(x => x.Username)
+                .NotEmpty().WithMessage("Nome de usuário é obrigatório")
+                .MinimumLength(4).WithMessage("O nome de usuário deve conter no mínimo 4 caracteres");
+
+            RuleFor(x => x.Password)
+                .NotEmpty().WithMessage("Senha é obrigatória")
+                .MinimumLength(3).WithMessage("A senha deve conter no mínimo 3 caracteres");
+        }
+    }
+}
